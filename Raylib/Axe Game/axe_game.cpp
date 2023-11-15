d7212afd3a0857175787943aa4f2e441ff1af58f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-15 20:38:52 +0300

    reversing axe direction (set boundaries)

--- Raylib/Axe Game/axe_game.cpp
+++ Raylib/Axe Game/axe_game.cpp
@@ -11,6 +11,7 @@ int main()
   int circle_y = 200;
   int axe_x = 400;
   int axe_y = 0;
+  int direction = 10;
 
   SetTargetFPS(60);
   while (!WindowShouldClose())
@@ -22,7 +23,11 @@ int main()
     DrawCircle(circle_x, circle_y, 25, BLUE);
     DrawRectangle(axe_x, axe_y, 50, 50, RED);
 
-    axe_y += 10;
+    axe_y += direction;
+    if (axe_y > height - 50 || axe_y < 0)
+    {
+      direction = -direction;
+    }
 
     if (IsKeyDown(KEY_D) && circle_x < 775)
     {
