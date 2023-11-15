f40530446fd7f089396f90722950b5542d7fbce8
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-15 20:20:10 +0300

    moving circle

--- Raylib/Axe Game/axe_game.cpp
+++ Raylib/Axe Game/axe_game.cpp
@@ -7,11 +7,29 @@ int main()
   int height = 200;
   InitWindow(width, height, "Another Life");
 
+  int circle_x = 175;
+  int circle_y = 100;
+
+  SetTargetFPS(60);
   while (!WindowShouldClose())
   {
     BeginDrawing();
     ClearBackground(WHITE);
-    DrawCircle(350 / 2, 200 / 2, 25, BLUE);
+
+    // Game logic begins
+    DrawCircle(circle_x, circle_y, 25, BLUE);
+
+    if (IsKeyDown(KEY_D))
+    {
+      circle_x += 10;
+    }
+
+    if (IsKeyDown(KEY_A))
+    {
+      circle_x -= 10;
+    }
+
+    // Game logic ends
     EndDrawing();
   }
 }
