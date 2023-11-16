e4e5a2002a2e11dee9adda09e70b08cd99ef3956
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-16 07:34:59 +0300

    add velocity and jumping

--- Raylib/2D Runner/runner.cpp
+++ Raylib/2D Runner/runner.cpp
@@ -6,11 +6,26 @@ int main()
   const int window_height = 380;
   InitWindow(window_width, window_height, "2D Runner");
 
+  const int width = 50;
+  const int height = 80;
+
+  int posY = window_height - height;
+  int velocity = 0;
+
   SetTargetFPS(60);
   while (!WindowShouldClose())
   {
     BeginDrawing();
     ClearBackground(WHITE);
+
+    if (IsKeyPressed(KEY_SPACE))
+    {
+      velocity = -10;
+    }
+    posY += velocity;
+
+    DrawRectangle(window_width / 2, posY, width, height, BLUE);
+
     EndDrawing();
   }
   CloseWindow();
