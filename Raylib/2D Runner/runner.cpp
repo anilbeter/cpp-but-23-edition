957acb16cc0f5828cdc0d81e029b3bfb824ee8d1
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-17 18:54:11 +0300

    replace AnimData variables

--- Raylib/2D Runner/runner.cpp
+++ Raylib/2D Runner/runner.cpp
@@ -30,14 +30,14 @@ int main()
   ilaydaData.updateTime = 1.0 / 12.0;
   ilaydaData.runningTime = 0.0;
 
-  Rectangle ilaydaRec;
-  ilaydaRec.width = ilayda.width / 6;
-  ilaydaRec.height = ilayda.height;
-  ilaydaRec.x = 0;
-  ilaydaRec.y = 0;
-  Vector2 ilaydaPos;
-  ilaydaPos.x = windowWidth / 2 - (ilaydaRec.width / 2);
-  ilaydaPos.y = windowHeight - ilaydaRec.height;
+  // Rectangle ilaydaRec;
+  // ilaydaRec.width = ilayda.width / 6;
+  // ilaydaRec.height = ilayda.height;
+  // ilaydaRec.x = 0;
+  // ilaydaRec.y = 0;
+  // Vector2 ilaydaPos;
+  // ilaydaPos.x = windowWidth / 2 - (ilaydaRec.width / 2);
+  // ilaydaPos.y = windowHeight - ilaydaRec.height;
 
   // Nebula variables
   Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
@@ -50,33 +50,14 @@ int main()
       1.0 / 12.0,
       0};
 
+  // AnimData for 2nd nebula
   AnimData neb2Data{{0, 0, (float)nebula.width / 8, (float)nebula.height / 8}, {windowWidth + 500, windowHeight - nebula.height / 8}, 0, 1.0 / 16.0, 0};
-
-  // x, y, width, height
-  Rectangle nebulaRec = {0, 0, (float)nebula.width / 8, (float)nebula.height / 8};
-  // x, y
-  Vector2 nebulaPos{windowWidth, windowHeight - nebulaRec.height};
-
-  Rectangle neb2Rec{0, 0, (float)nebula.width / 8, (float)nebula.height / 8};
-  Vector2 neb2Pos{windowWidth + 500, windowHeight - nebulaRec.height};
 
   int nebulaVel = -200;
 
   int velocity = 0;
   bool isInAir;
   const int jumpVelocity = -600;
-
-  int frame;
-  const float updateTime = 1.0 / 12.0;
-  float runningTime;
-
-  int nebFrame;
-  const float nebUpdateTime = 1.0 / 12.0;
-  float nebRunningTime;
-
-  int neb2Frame;
-  const float neb2UpdateTime = 1.0 / 16.0;
-  float neb2RunningTime;
 
   SetTargetFPS(60);
   while (!WindowShouldClose())
@@ -85,7 +66,7 @@ int main()
     ClearBackground(WHITE);
     const float dT = GetFrameTime();
 
-    if (ilaydaPos.y > windowHeight - ilaydaRec.height)
+    if (ilaydaData.pos.y > windowHeight - ilaydaData.rec.height)
     {
       velocity = 0;
       isInAir = false;
@@ -102,59 +83,59 @@ int main()
     }
 
     // Update Ilayda position
-    ilaydaPos.y += velocity * dT;
+    ilaydaData.pos.y += velocity * dT;
 
     // Update Nebula position
-    nebulaPos.x += nebulaVel * dT;
+    nebData.pos.x += nebulaVel * dT;
 
     // Update second nebula pos
-    neb2Pos.x += nebulaVel * dT;
+    neb2Data.pos.x += nebulaVel * dT;
 
     // Ilayda animation
     if (!isInAir)
     {
-      runningTime += dT;
-      if (runningTime >= updateTime)
+      ilaydaData.runningTime += dT;
+      if (ilaydaData.runningTime >= ilaydaData.updateTime)
       {
-        runningTime = 0.0;
+        ilaydaData.runningTime = 0.0;
 
-        ilaydaRec.x = frame * ilaydaRec.width;
-        frame++;
-        if (frame > 5)
+        ilaydaData.rec.x = ilaydaData.frame * ilaydaData.rec.width;
+        ilaydaData.frame++;
+        if (ilaydaData.frame > 5)
         {
-          frame = 0;
+          ilaydaData.frame = 0;
         }
       }
     }
 
     // Nebula animation
-    nebRunningTime += dT;
-    if (nebRunningTime >= nebUpdateTime)
+    nebData.runningTime += dT;
+    if (nebData.runningTime >= nebData.updateTime)
     {
-      nebRunningTime = 0.0;
-      nebulaRec.x = nebFrame * nebulaRec.width;
-      nebFrame++;
-      if (nebFrame > 7)
+      nebData.runningTime = 0.0;
+      nebData.rec.x = nebData.frame * nebData.rec.width;
+      nebData.frame++;
+      if (nebData.frame > 7)
       {
-        nebFrame = 0;
+        nebData.frame = 0;
       }
     }
 
-    neb2RunningTime += dT;
-    if (neb2RunningTime >= neb2UpdateTime)
+    neb2Data.runningTime += dT;
+    if (neb2Data.runningTime >= neb2Data.updateTime)
     {
-      neb2RunningTime = 0.0;
-      neb2Rec.x = neb2Frame * neb2Rec.width;
-      neb2Frame++;
-      if (neb2Frame > 7)
+      neb2Data.runningTime = 0.0;
+      neb2Data.rec.x = neb2Data.frame * neb2Data.rec.width;
+      neb2Data.frame++;
+      if (neb2Data.frame > 7)
       {
-        neb2Frame = 0;
+        neb2Data.frame = 0;
       }
     }
 
-    DrawTextureRec(ilayda, ilaydaRec, ilaydaPos, WHITE);
-    DrawTextureRec(nebula, nebulaRec, nebulaPos, WHITE);
-    DrawTextureRec(nebula, neb2Rec, neb2Pos, RED);
+    DrawTextureRec(ilayda, ilaydaData.rec, ilaydaData.pos, WHITE);
+    DrawTextureRec(nebula, nebData.rec, nebData.pos, WHITE);
+    DrawTextureRec(nebula, neb2Data.rec, neb2Data.pos, RED);
 
     EndDrawing();
   }
