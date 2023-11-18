c447e3b95896d9fd2fea9f227015de3898a7f345
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-18 10:50:03 +0300

    draw midground and foreground

--- Raylib/2D Runner/runner.cpp
+++ Raylib/2D Runner/runner.cpp
@@ -84,6 +84,12 @@ int main()
   Texture2D background = LoadTexture("textures/far-buildings.png");
   float bgX{};
 
+  Texture2D midground = LoadTexture("textures/back-buildings.png");
+  float mgX{};
+
+  Texture2D foreground = LoadTexture("textures/foreground.png");
+  float fgX{};
+
   SetTargetFPS(60);
   while (!WindowShouldClose())
   {
@@ -97,10 +103,32 @@ int main()
       bgX = 0.0;
     }
 
+    mgX -= 40 * dT;
+    if (mgX <= -midground.width * 2)
+    {
+      mgX = 0;
+    }
+
+    fgX -= 80 * dT;
+    if (fgX <= -foreground.width * 2)
+    {
+      fgX = 0;
+    }
+
     Vector2 bg1Pos{bgX, 0.0};
     DrawTextureEx(background, bg1Pos, 0.0, 2.0, WHITE);
     Vector2 bg2Pos{bgX + background.width * 2, 0.0};
     DrawTextureEx(background, bg2Pos, 0.0, 2.0, WHITE);
+
+    Vector2 mg1Pos{mgX, 0.0};
+    DrawTextureEx(midground, mg1Pos, 0.0, 2.0, WHITE);
+    Vector2 mg2Pos{mgX + midground.width * 2, 0.0};
+    DrawTextureEx(midground, mg2Pos, 0.0, 2.0, WHITE);
+
+    Vector2 fg1Pos{fgX, 0.0};
+    DrawTextureEx(foreground, fg1Pos, 0.0, 2.0, WHITE);
+    Vector2 fg2Pos{fgX + foreground.width * 2, 0.0};
+    DrawTextureEx(foreground, fg2Pos, 0.0, 2.0, WHITE);
 
     if (isOnGround(ilaydaData, windowDimensions[1]))
     {
@@ -149,5 +177,7 @@ int main()
   UnloadTexture(ilayda);
   UnloadTexture(nebula);
   UnloadTexture(background);
+  UnloadTexture(midground);
+  UnloadTexture(foreground);
   CloseWindow();
 }
