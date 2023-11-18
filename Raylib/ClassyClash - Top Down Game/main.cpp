59b025e021f777e93878517f2d4ffc780c9bd5a9
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-18 15:14:18 +0300

    draw the character

--- Raylib/ClassyClash - Top Down Game/main.cpp
+++ Raylib/ClassyClash - Top Down Game/main.cpp
@@ -8,8 +8,12 @@ int main()
 
   Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
   Vector2 mapPos{0.0, 0.0};
-
   float speed = 4.0;
+
+  Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+  Vector2 knightPos{
+      (float)windowDimensions[0] / 2.0f - 4.0f * (0.5f * (float)knight.width / 6.0f),
+      (float)windowDimensions[1] / 2.0f - 4.0f * (0.5f * (float)knight.height)};
 
   SetTargetFPS(60);
   while (!WindowShouldClose())
@@ -34,7 +38,13 @@ int main()
       mapPos = Vector2Subtract(mapPos, scaledVector);
     }
 
+    // draw the map
     DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+    // draw the character
+    Rectangle source{0.f, 0.f, knight.width / 6.f, (float)knight.height};
+    Rectangle dest{knightPos.x, knightPos.y, 4.0f * knight.width / 6.0f, 4.0f * knight.height};
+    DrawTexturePro(knight, source, dest, Vector2{}, 0.f, WHITE);
 
     EndDrawing();
   }
