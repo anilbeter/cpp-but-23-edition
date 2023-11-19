f3c375d95f2702a90045c6a5dd93f82ece07948b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-11-19 15:44:01 +0300

    create tick and setScreenPos functions

--- Raylib/ClassyClash - Top Down Game/main.cpp
+++ Raylib/ClassyClash - Top Down Game/main.cpp
@@ -4,13 +4,14 @@
 class Character
 {
 public:
-  // Getter method
   Vector2 getWorldPos() { return worldPos; }
+  void setScreenPos(int winWidth, int winHeight);
+  void tick(float deltaTime);
 
 private:
-  Texture2D texture;
-  Texture2D idle;
-  Texture2D run;
+  Texture2D texture = LoadTexture("characters/knight_idle_spritesheet.png");
+  Texture2D idle = LoadTexture("characters/knight_idle_spritesheet.png");
+  Texture2D run = LoadTexture("characters/knight_run_spritesheet.png");
   Vector2 screenPos;
   Vector2 worldPos;
   float rightLeft = 1.f;
@@ -18,6 +19,49 @@ private:
   int frame;
   const int maxFrames = 6;
   const float updateTime = 1.0f / 12.0f;
+  const float speed = 4.f;
+};
+
+void Character::setScreenPos(int winWidth, int winHeight)
+{
+  screenPos = {
+      (float)winWidth / 2.0f - 4.0f * (0.5f * (float)texture.width / 6.0f),
+      (float)winHeight / 2.0f - 4.0f * (0.5f * (float)texture.height)};
+};
+
+void Character::tick(float deltaTime)
+{
+  Vector2 direction;
+  if (IsKeyDown(KEY_A))
+    direction.x -= 1.0;
+  if (IsKeyDown(KEY_D))
+    direction.x += 1.0;
+  if (IsKeyDown(KEY_W))
+    direction.y -= 1.0;
+  if (IsKeyDown(KEY_S))
+    direction.y += 1.0;
+  if (Vector2Length(direction) != 0.0)
+  {
+    // set worldPos = worldPos + direction
+
+    worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+    direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+    texture = run;
+  }
+  else
+  {
+    texture = idle;
+  }
+
+  // update animation frame
+  runningTime += deltaTime;
+  if (runningTime >= updateTime)
+  {
+    frame++;
+    runningTime = 0.f;
+    if (frame > maxFrames)
+      frame = 0;
+  }
 };
 
 int main()
